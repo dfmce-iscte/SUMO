0f17bf0bbcd0549e19df1d5a6f32ad7b6f132459
Pedro Afonso Marques Oliveira <pamdo@iscte-iul.pt>
2023-12-19 17:45:43 +0100

    New road

--- main.py
+++ main.py
@@ -5,86 +5,51 @@ import traci
 def run_sumo():
     sumoBinary = "C:\Program Files (x86)\Eclipse\Sumo\\bin\sumo-gui.exe"
     sumoBinary = sumoBinary.replace("\\", "/")
-    sumoCmd = [sumoBinary, "-c", "config_project.sumocfg"]
+    sumoCmd = [sumoBinary, "-c", "final.sumocfg"]
 
     traci.start(sumoCmd)
-
     step = 0
-    before_junction = "before_junction"
-    ramp = "ramp"
+    before_junction = "E1"
+    ramp = "E4"
+    after_junction = "E2"
     # Get the number of lanes for the specified edge
     num_lanes_before_junction = traci.edge.getLaneNumber(before_junction)
     num_lanes_ramp = traci.edge.getLaneNumber(ramp)
+    num_lanes_after_junction = traci.edge.getLaneNumber(after_junction)
 
-    # Initialize the total width
-    total_width_before_junction = 0
-    total_width_ramp = 0
+    area_of_before_junction = calculate_area(before_junction, num_lanes_before_junction)
+    print(f"Area of edge {before_junction}: {area_of_before_junction} meters")
 
-    # Iterate through each lane and add its width to the total
-    for lane_index in range(num_lanes_before_junction):
-        lane_id = f"{before_junction}_{lane_index}"
-        lane_width = traci.lane.getWidth(lane_id)
-        total_width_before_junction += lane_width
+    area_of_after_junction = calculate_area(ramp,num_lanes_ramp)
+    print(f"Area of edge {after_junction}: {area_of_after_junction} meters")
 
-    # Iterate through each lane and add its width to the total
-    for lane_index in range(num_lanes_ramp):
-        lane_id = f"{ramp}_{lane_index}"
-        lane_width = traci.lane.getWidth(lane_id)
-        total_width_ramp += lane_width
-
-    area_of_junction = total_width_before_junction * traci.lane.getLength(before_junction + "_0")
-    print(f"Area of edge {before_junction}: {area_of_junction} meters")
-
-    area_of_ramp = traci.lane.getLength(ramp + "_0") * total_width_ramp
+    area_of_ramp = calculate_area(after_junction,num_lanes_after_junction)
     print(f"Area of edge {ramp}: {area_of_ramp} meters")
     while step < 1000:
-        before_junction = traci.edge.getLastStepVehicleIDs("before_junction")
-        ramp = traci.edge.getLastStepVehicleIDs("ramp")
+        before_junction = traci.edge.getLastStepVehicleIDs("E1")
+        ramp = traci.edge.getLastStepVehicleIDs("E4")
+        after_junction = traci.edge.getLastStepVehicleIDs("E2")
         sumOfAreaOfCarsInBeforeJunction = 0
         sumOfAreaOfCarsInRamp = 0
+        sumOfAreaOfcarsAfterJunction = 0
         for x in before_junction:
             # print(traci.vehicle.getTypeID(x))
             # print(traci.vehicle.getWidth(x) * traci.vehicle.getLength(x))
             sumOfAreaOfCarsInBeforeJunction += traci.vehicle.getWidth(x) * traci.vehicle.getLength(x)
-        print(f"Density of cars in edge before_junction: {sumOfAreaOfCarsInBeforeJunction / area_of_junction}")
+        print(f"Density of cars in edge before_junction: {sumOfAreaOfCarsInBeforeJunction / area_of_before_junction}")
 
         for y in ramp:
             # print(traci.vehicle.getWidth(y) * traci.vehicle.getLength(y))
             sumOfAreaOfCarsInRamp += traci.vehicle.getWidth(y) * traci.vehicle.getLength(y)
         print(f"Density of cars in edge ramp: {sumOfAreaOfCarsInRamp / area_of_ramp}")
 
+        for wyd in after_junction:
+            sumOfAreaOfcarsAfterJunction += traci.vehicle.getWidth(wyd) * traci.vehicle.getLength(wyd)
+        print(f"Density of cars in edge ramp: {sumOfAreaOfcarsAfterJunction / area_of_after_junction}")
+
         traci.simulationStep()
         generate_random_vehicles(step)
-        # if step == 0:
-        #     traci.vehicle.add("v1", typeID="bus", routeID="r_0", departLane=2, departSpeed=20)
-        #     traci.vehicle.add("v0", typeID="car", routeID="r_0", departLane=0, departSpeed=20)
-        #     traci.vehicle.add("v2", typeID="motorcycle", routeID="r_0", departLane=1, departSpeed=20)
-        #     traci.vehicle.add("v3", typeID="truck", routeID="r_0", departLane=1, departSpeed=20)
-        #     traci.vehicle.add("v4", typeID="emergency", routeID="r_0", departLane=1, departSpeed=20)
-        # elif step == 10:
-        #     traci.vehicle.add("v3", routeID="r_0", departLane=0, departSpeed=20)
-        #     traci.vehicle.add("v5", routeID="r_0", departLane=2, departSpeed=20)
-        #     traci.vehicle.add("v16", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v17", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v18", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v19", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v20", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v21", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v22", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v23", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v24", routeID="r_0", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v25", routeID="r_0", departLane=0, departSpeed=14)
-        # elif step == 17:
-        #     traci.vehicle.add("v4", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v7", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v8", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v9", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v10", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v11", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v12", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v13", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v14", routeID="r_1", departLane=0, departSpeed=14)
-        #     traci.vehicle.add("v15", routeID="r_1", departLane=0, departSpeed=14)
+
         step += 1
 
     traci.close()
@@ -116,6 +81,17 @@ def generate_random_vehicles(step):
         traci.vehicle.add(str(step) + "vehicleRamp" + str(i), typeID=vehicle_type, routeID="r_1", departLane=0,
                           departSpeed=depart_speed_ramp)
         i += 1
+
+
+def calculate_area(road, num_lanes):
+    total_width = 0.0
+
+    for lane_index in range(num_lanes):
+        lane_id = f"{road}_{lane_index}"
+        lane_width = traci.lane.getWidth(lane_id)
+        total_width += lane_width
+
+    return total_width * traci.lane.getLength(road + "_0")
 
 
 def main():
